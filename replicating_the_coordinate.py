ed947ac9beea3427169b22f56d09bd422964f613
Kei Ishikura <keiishikura@ww070085.uvt.nl>
2026-02-06 21:53:41 +0100

    change in speed

--- replicating_the_coordinate.py
+++ replicating_the_coordinate.py
@@ -5,11 +5,11 @@ import csv
 
 csv_path = "demo_mycobot.csv"
 
-speed = 70
+speed = 60
 use_time_stamps = True
 fixed_dt = 0.1
-max_dt = 0.3
-min_dt = 0.04
+max_dt = 0.1
+min_dt = 0.1
 
 trajectory_joint_angle = True
 trajectory_coords = False

6ac331f7cbe1d63796c935dde6844cfb6aaa98b7
Timotx23 <thildenbrand@web.de>
2026-02-06 09:45:32 +0100

    Timos'code

--- testing_robot.py
+++ testing_robot.py
@@ -23,6 +23,13 @@ print("Moving Joint 2...")
 mc.send_angle(2, 40, 50)
 time.sleep(2)
 
+mc.send_angles([0, 0, 0, 0, 0, 0], 50)
+time.sleep(3) # Give it time to move
+print("Moving joint 3..")
+mc.send_angle(1, 160,100)
+time.sleep(3)
+mc.send_angles([0,0,0, 0,0,0],50)
+
 # 5. Get current state
 angles = mc.get_angles()
 print(f"Current Joint Angles: {angles}")

68d1ce15369a6e6708634ba7b086b6db365f661b
Kei Ishikura <keiishikura@ww070085.uvt.nl>
2026-02-06 19:08:41 +0100

    Fixed the range to -180 to 180

--- trajectory_smoothener.py
+++ trajectory_smoothener.py
@@ -3,7 +3,7 @@ from scipy.signal import savgol_filter
 import csv
 
 window_size = 11
-poly_order = 3
+poly_order = 4
 
 filename = 'Savitzky_Golay_denoised.csv'
 
@@ -32,12 +32,20 @@ with open('demo_mycobot.csv', 'r') as csv_file:
 
     csv_file.close()
 
+def smooth_angle_array(angle_list, window, poly):
+    rads = np.deg2rad(angle_list)
+    unwrapped = np.unwrap(rads)
+    smoothed = savgol_filter(unwrapped, window, poly)
+    degs = np.rad2deg(smoothed)
+    return (degs + 180) % 360 - 180 #back to -180, 180 range
+
+
 x_smoothener = savgol_filter(x, window_size, poly_order)
 y_smoothener = savgol_filter(y, window_size, poly_order)
 z_smoothener = savgol_filter(z, window_size, poly_order)
-rx_smoothener = savgol_filter(rx, window_size, poly_order)
-ry_smoothener = savgol_filter(ry, window_size, poly_order)
-rz_smoothener = savgol_filter(rz, window_size, poly_order)
+rx_smoothener = smooth_angle_array(rx, window_size, poly_order)
+ry_smoothener = smooth_angle_array(ry, window_size, poly_order)
+rz_smoothener = smooth_angle_array(rz, window_size, poly_order)
 
 clean_data = zip(timestamp, x_smoothener, y_smoothener, z_smoothener, rx_smoothener, ry_smoothener, rz_smoothener, gripper_val)
 

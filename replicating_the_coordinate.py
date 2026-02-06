0a1ffd9cd6e2fa407fedf04084ae6c3e0b5321fd
Kei Ishikura <keiishikura@ww070085.uvt.nl>
2026-02-06 16:15:19 +0100

    Changed the robot to reply on coordinate system, not on joints

--- replicating_the_coordinate.py
+++ replicating_the_coordinate.py
@@ -11,6 +11,7 @@ fixed_dt = 0.10
 max_dt = 0.30
 min_dt = 0.02
 
+
 def loading_trajectory(path):
     traj = []
     with open(path, newline="") as f:
@@ -25,23 +26,45 @@ def loading_trajectory(path):
         raise ValueError("CSV is probably empty")
     return traj
 
+
+def loading_trajectory_coord(path):
+    traj = []
+    with open(path, newline="") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            ts = float(row["timestamp"])
+            coordinate = []
+            coordinate.append(float(row['x']))
+            coordinate.append(float(row['y']))
+            coordinate.append(float(row['z']))
+            coordinate.append(float(row['rx']))
+            coordinate.append(float(row['ry']))
+            coordinate.append(float(row['rz']))
+            gripper_pos = float(row['gripper'])
+            traj.append((ts, coordinate, gripper_pos))
+    if not traj:
+        raise ValueError("CSV is probably empty")
+    return traj
+            
+
+
 def main():
     mc = MyCobot280(PI_PORT, PI_BAUD)
 
-    traj = loading_trajectory(csv_path)
+    traj = loading_trajectory_coord(csv_path)
     print(f"Loaded {len(traj)} frames from {csv_path}")
 
-    # Optional: go to first frame before replay
     mc.power_on()
     time.sleep(0.5)
-    first_angles = traj[0][1]
+    ts0, coordinate0, gripper0 = traj[0]
     print("Going to first recorded pose...")
-    mc.send_angles(first_angles, speed)
+    mc.send_coords(coordinate0, speed, 0)
     time.sleep(2)
+    mc.set_gripper_value(int(gripper0), speed)
 
     try:
-        prev_ts = traj[0][0]
-        for ts, angles in traj:
+        prev_ts = ts0
+        for ts, coords, g in traj:
             # Timing
             if use_time_stamps:
                 dt = ts - prev_ts
@@ -52,7 +75,8 @@ def main():
                 dt = fixed_dt
 
             # Command
-            mc.send_angles(angles, speed)
+            mc.send_coords(coords, speed, 0) # mode = 0
+            mc.set_gripper_value(int(g), speed)
 
             # Wait roughly the recorded interval
             time.sleep(dt)

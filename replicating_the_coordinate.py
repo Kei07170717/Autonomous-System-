2ae4fa8cbfdbe8d5517db7d5cb49f6629b769251
Kei Ishikura <keiishikura@ww070085.uvt.nl>
2026-02-12 21:46:38 +0100

    Increase chunk size

--- replicating_the_coordinate.py
+++ replicating_the_coordinate.py
@@ -14,7 +14,7 @@ fixed_dt = T
 max_dt = 0.30
 min_dt = 0.04
 
-chunk_size = 25
+chunk_size = 50
 gripper_interval = 25
 gripper_threshold = 50
 
@@ -148,16 +148,16 @@ def main():
                     mc.send_angles(angles, speed)
                     
                     
-                    # if gripper_binary != last_gripper_val:
-                    #     if gripper_steps_counter >= gripper_interval:
-                    #         time.sleep(0.02) 
-                    #         mc.set_gripper_value(int(gripper_binary), speed)
-                    #         time.sleep(0.02)  
+                    if gripper_binary != last_gripper_val:
+                        if gripper_steps_counter >= gripper_interval:
+                            time.sleep(0.02) 
+                            mc.set_gripper_value(int(gripper_binary), speed)
+                            time.sleep(0.02)  
                             
-                    #         last_gripper_val = gripper_binary
-                    #         gripper_steps_counter = 0 
+                            last_gripper_val = gripper_binary
+                            gripper_steps_counter = 0 
                     
-                    # gripper_steps_counter += 1
+                    gripper_steps_counter += 1
                     
                     wait = next_step_time - time.time()
                     if wait > 0:

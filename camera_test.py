b3cb570558c4540e5673054c8abc6865a6b21ae1
Kei Ishikura <keiishikura@ww070085.uvt.nl>
2026-02-13 13:20:46 +0100

    Confirming camera operation:

--- camera_test.py
+++ camera_test.py
@@ -1,12 +1,17 @@
 import cv2
 
-for i in [0,1,2,3]:
-    cap = cv2.VideoCapture(i)
-    ret, frame = cap.read()
-    if ret:
-        print(f"/dev/video{i} works")
-    else:
-        print(f"/dev/video{i} no frame")
-    cap.release()
+cap0 = cv2.VideoCapture(0)
+cap2 = cv2.VideoCapture(2)
 
+while True:
+    _, f0 = cap0.read()
+    _, f2 = cap2.read()
+
+    cv2.imshow("cam0", f0)
+    cv2.imshow("cam2", f2)
+
+    if cv2.waitKey(1)==27:
+        break
+
+    
 

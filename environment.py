31894337380d2227fecb262f807fa5a1cd231b24
Kei Ishikura <keiishikura@ww070085.uvt.nl>
2026-03-06 12:51:43 +0100

    Development on the environment

--- environment.py
+++ environment.py
@@ -1,3 +1,60 @@
-import dm_env
-from dm_env import specs
+import dm_env 
+from dm_env import specs, _environment
 import numpy as np
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+
+class Environment(dm_env.Environment):
+    def reset(self) -> TimeStep:
+        pass
+
+    def observation_spec(self):
+        pass
+
+    def action_spec(self):
+        pass
+
+
+class RecordedEnvironment(dm_env.Environment, ABC):
+    def __init__(self, env: dm_env.Environment):
+        self.env: dm_env.Environment = env
+
+    @abstractmethod
+    def reset(self) -> TimeStep:
+        pass
+
+    @abstractmethod
+    def step(self, action) -> TimeStep:
+        pass
+    
+    def reward_spec(self):
+        pass
+
+    def discount_spec(self):
+        pass
+
+    @abstractmethod
+    def observation_spec(self):
+        pass
+
+    @abstractmethod
+    def action_spec(self):
+        pass
+
+    def close(self):
+        pass
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+
+class RLDSRecordedEnvironment(RecordedEnvironment):
+    def __init__(self):
+        super().__init__()
+        
+
+

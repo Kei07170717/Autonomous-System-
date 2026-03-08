c81c8cdcf02366f584d4a18fe89cb55eb7185e67
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2026-03-08 12:08:40 +0100

    Added abstract and KinestheticAgent

---
+++ agent-env-core/agent/agent.py
@@ -0,0 +1,22 @@
+
+from abc import ABC, abstractmethod
+from environment import Action
+
+class Agent(ABC):
+
+
+    @abstractmethod
+    def get_action(obs: dict) -> Action:
+        pass
+
+
+
+class KinestheticAgent(Agent):
+    '''This agent is passive, the actions it sends are mirrored from
+    the observation. This agent is used for gathering 'drag&record' data,
+    which can later be replayed through another agent. The environment should
+    not exectute actions using this agent (observe_only mode).'''
+
+    def get_action(obs: dict) -> Action:
+        # TODO: return observed angles
+        return Action([0, 0, 0, 0, 0, 0])

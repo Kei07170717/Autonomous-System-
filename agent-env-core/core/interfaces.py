eac51b4dc6918c416c5129015cc09bf827b8a947
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2026-03-08 17:56:07 +0100

    Added IObserver to core.interfaces, moved sensors.py to environment/camera.py

--- agent-env-core/core/interfaces.py
+++ agent-env-core/core/interfaces.py
@@ -1,8 +1,11 @@
 from .types import Action
 from abc import ABC, abstractmethod
 
+"""Contains the core interfaces that the application depends on."""
+
 
 class IBody(ABC):
+    """Capable of affecting the world."""
     @abstractmethod
     def affect_world(self, action: Action) -> None:
         # Apply action to the world
@@ -10,13 +13,23 @@ class IBody(ABC):
 
 
 class ISensor(ABC):
-    '''Interface for all sensors'''
+    """Interface for all sensors"""
     @abstractmethod
     def get_data(self) -> any:
         pass
 
 
+# TODO: should it be an interface or an abstract class?
 class Agent(ABC):
     @abstractmethod
-    def get_action(obs: dict) -> Action:
+    def get_action(self, obs: dict) -> Action:
+        pass
+
+
+class IObserver(ABC):
+    """
+    Responsible for managing sensors and returning a 'formal' observation.
+    """
+    @abstractmethod
+    def get_observation(self) -> dict:
         pass

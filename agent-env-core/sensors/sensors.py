ec961512060758832187bac4f574b986ed05fb6f
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2026-03-08 16:20:10 +0100

    Fixed wrong opencv import

--- agent-env-core/sensors/sensors.py
+++ agent-env-core/sensors/sensors.py
@@ -1,13 +1,13 @@
 import time 
 import numpy as np
-import cv
+import cv2 as cv
 from pymycobot.mycobot280 import MyCobot280
 from pymycobot import PI_PORT, PI_BAUD
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Optional, Sequence 
 import torch
-from body import RobotStateProvider
+# from body import RobotStateProvider
 
 
 class Sensor(ABC): 
@@ -26,9 +26,10 @@ class Sensor(ABC):
 
 
 class Camera(Sensor):
-    def __init__(self, path: str, id: str):
+    def __init__(self, id: str, path: str):
         super().__init__(self, id = id)
         self.path = path
+        self.capture = cv.VideoCapture(id)
      
     def get_tensorized_frame(self):
         """This function will process the frame and turn it into a tensor"""
@@ -38,7 +39,7 @@ class Camera(Sensor):
             return tensorized_frame
             #cv.imshow('Video Not Resized', frame)# This is just to show the video but isnt needed  
         else:
-            raise ValueError("Frame Doesn't exist")
+            raise RuntimeError("Frame doesn't exist")
 
     def convert_to_tensor(self,frame):
         """This function will convert the frame to a tensor"""
@@ -54,20 +55,20 @@ class Camera(Sensor):
         #cv.destroyAllWindows()
 
 
-class Proprioceptive(Sensor):
-    def __init__(
-            self, 
-            id: str, 
-            state: RobotStateProvider):
-        
-        super().__init__(self, id=id)
-        self.state = state
-
-    def get_data(self):
-        return {
-            "joint_angles": self.state.get_joint_angles(),
-            "gripper_state": self.state.get_gripper_value()
-        }
+# class Proprioceptive(Sensor):
+#     def __init__(
+#             self, 
+#             id: str, 
+#             state: RobotStateProvider):
+#
+#         super().__init__(self, id=id)
+#         self.state = state
+#
+#     def get_data(self):
+#         return {
+#             "joint_angles": self.state.get_joint_angles(),
+#             "gripper_state": self.state.get_gripper_value()
+#         }
 
 """
 when defining proprioceptive,

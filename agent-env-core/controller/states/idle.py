6a9ad4edae8e2740dbc77481080f4d3c2bd2dedc
Bartinos <bartvanmoorsel7@gmail.com>
2026-03-10 20:12:07 +0100

    Added dummy idle state

---
+++ agent-env-core/controller/states/idle.py
@@ -0,0 +1,33 @@
+from ..interfaces import State, IANCController
+
+class IdlingState(State):
+  
+    def __init__(self, context: IANCController):
+        super().__init__(context)
+
+    def execute(self):
+        pass
+
+    def open_gripper(self):
+        pass
+
+    def close_gripper(self):
+        pass
+
+    def start_replay_record(self):
+        pass
+
+    def stop_replay_record(self):
+        pass
+
+    def start_drag_record(self):
+        pass
+
+    def stop_drag_record(self):
+        pass
+
+    # def start_inference(self):
+    #     pass
+    #
+    # def stop_inference(self):
+    #     pass

9fdea822f94a7ce018992773f813cbb7e6dca0dd
Bartinos <bartvanmoorsel7@gmail.com>
2026-03-10 18:06:48 +0100

    Added dummy class that fakes arm/gripper movement

--- agent-env-core/hardware/dummy_components.py
+++ agent-env-core/hardware/dummy_components.py
@@ -1,3 +1,15 @@
-from core.interfaces import IBody
+from core.interfaces import IArmActuator, IGripperActuator 
 
+class DummyComponent(IArmActuator, IGripperActuator):
 
+    def set_joint_angles(self, arm_pos: list[float]):
+        pass
+
+    def set_gripper_closed(self) -> None:
+        pass
+
+    def set_gripper_open(self) -> None:
+        pass
+
+    def set_gripper_value(self, value) -> None:
+        pass

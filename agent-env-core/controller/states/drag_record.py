f918b591c4132ce33cf0b0796c613d787b382f39
Bartinos <bartvanmoorsel7@gmail.com>
2026-03-11 15:20:44 +0100

    Added empty DragRecording state

---
+++ agent-env-core/controller/states/drag_record.py
@@ -0,0 +1,34 @@
+from ..interfaces import State, IANCController
+
+class DragRecordingState(State):
+    """
+    In this state, the arm should go limp and interaction between
+    KinestheticAgent and a child class of RecordedEnvironment should happen.
+    This makes it so that the interactions are recorded automatically. 
+    This state is meant to be spawned from the IdlingState and transition to
+    the ResettingState when done or terminated.
+    """
+  
+    def __init__(self, context: IANCController):
+        super().__init__(context, state_name="DragRecord")
+
+    def execute(self):
+        pass
+
+    def open_gripper(self):
+        pass
+
+    def close_gripper(self):
+        pass
+
+    def start_replay_record(self):
+        pass
+
+    def stop_replay_record(self):
+        pass
+
+    def start_drag_record(self):
+        pass
+
+    def stop_drag_record(self):
+        pass

b95c0a3f78bd54511491b7fe4e166ff38b4ca71f
jeltescheffel06 <jelte@scheffel.me>
2026-03-12 13:30:58 +0100

    added some angles

--- recording/actual-angledifference.py
+++ recording/actual-angledifference.py
@@ -5,8 +5,10 @@ from pymycobot import PI_PORT, PI_BAUD
 mc = MyCobot280(PI_PORT, PI_BAUD)
 mc.send_angles([50,0,0,0,0,0], 50)
 time.sleep(1)
-mc.send_angles([0,0,0,0,0,0], 50)
+mc.send_angles([50,50,0,0,0,0], 50)
 time.sleep(1)
+mc.send_angles([0,0,0,0,0,0], 50)
+time.sleep(2)
 angles = mc.get_angles()
 print(angles)
 

f7a88f4aadb30ef6d5978a185d14c05ad774494e
Kei Ishikura <keiishikura@ww070085.uvt.nl>
2026-03-19 15:42:00 +0100

    Configs.py has been adjusted for mycobto280pi

--- openvla-oft/prismatic/vla/datasets/rlds/oxe/configs.py
+++ openvla-oft/prismatic/vla/datasets/rlds/oxe/configs.py
@@ -706,4 +706,12 @@ OXE_DATASET_CONFIGS = {
         "state_encoding": StateEncoding.JOINT_BIMANUAL,
         "action_encoding": ActionEncoding.JOINT_POS_BIMANUAL,
     },
+    "my_cobot_280_pi": {
+	"image_obs_keys": {"primary": "cam1", "secondary": None, "wrist": "image_wrist"}, #RGB (H,W,3) uint8 (placeholder wrist)
+	"depth_obs_keys": {"primary": None, "secondary": None, "wrist": None}, #RBG (H,W,3) uint8
+	"state_obs_keys": ["arm_angles", "gripper"],
+	"state_encoding": StateEncoding.JOINT,
+	"action_encoding": ActionEncoding.JOINT_POS
+    }
+
 }

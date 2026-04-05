6f40d17ac4579683e4e70a9570a3bab37e73590c
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2026-04-05 12:10:13 +0200

    Set camera hardware res and fps to 352x288 @ 30

--- agent-env-core/hardware/camera.py
+++ agent-env-core/hardware/camera.py
@@ -17,16 +17,37 @@ class NullFrameReturnedError(Exception):
     """Raised when an empty/null frame is read."""
     pass
 
+# Globals because setting them as params is too tricky; 
+# if someone changes these, it must be communicated with the team!
+# Having this changed between demonstrations in the dataset can cause problems
+_RES_WIDTH = 352 
+_RES_HEIGHT = 288
+_TARGET_FPS = 30
 class Camera(ICameraSensor):
     """
     This class gets as input the camera name(example names are in get_camera_path docstring) 
-    and returns either the actual frame or a tensorized version of the frame
+    and returns either the actual frame or a tensorized version of the frame.
+    Camera class should be modified with care as transformation of the frames should be 
+    the same during gathering demonstrations and inference. 
     """
     def __init__(self, camera_name: str):
         self.camera_name: str = camera_name
         self.os: int = self._get_os()
         self.path: int = self.get_camera_path()
         self.capture = cv2.VideoCapture(self.path, self.os)
+                
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, _RES_WIDTH)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, _RES_HEIGHT)
+        self.capture.set(cv2.CAP_PROP_FPS, _TARGET_FPS)
+
+        try:
+            self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 1) # Makes sure only the latest frame can be read (probably not necessary)
+        except Exception:
+            print(f"Warning: couldn't set camera buffer to 1 frame for cam: {camera_name}")
+
+        if not self._verify_set_res_and_fps():
+            print(f"Warning: actual camera resolution or FPS do not match set target for cam: {self.camera_name}")
+
         self.latest_frame = None
         self.capture_failed = False
         self.lock = threading.Lock()
@@ -75,7 +96,16 @@ class Camera(ICameraSensor):
                     print(f"Warning: Index {cam.index} matched but failed to open.")
 
         raise ValueError(f"Camera '{self.camera_name}' could not be found or opened for use.")
-    
+   
+    def _verify_set_res_and_fps(self):
+        actual_w = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        actual_h = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        actual_fps = self.capture.get(cv2.CAP_PROP_FPS)
+
+        if actual_h != _RES_HEIGHT or actual_w != _RES_WIDTH or actual_fps != _TARGET_FPS:
+            return False
+        return True
+
     def update_frames(self):
         """Continuously grab frames for the thread"""
         while not self.stop_event.is_set(): 

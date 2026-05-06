fbe7532a69c485886d2f79cecd6c894c3728a5aa
Timotx23 <t.g.hildenbrand@tilburguniversity.edu>
2026-05-06 14:10:54 +0200

    Even better improved colors

--- cv/main.py
+++ cv/main.py
@@ -7,7 +7,7 @@ from visual_perceptor import VisualPerceptor
 def main():
     print('Starting...')
     cobot: Cobot = Cobot()
-    visual_perceptor: VisualPerceptor = VisualPerceptor(mm_per_pixel=0.09, debug=True)
+    visual_perceptor: VisualPerceptor = VisualPerceptor(mm_per_pixel=0.09, debug=False)
     controller: StackController = StackController(cobot, visual_perceptor) 
 
     controller.start()
